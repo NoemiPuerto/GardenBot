22f333d8ac03a3da16a1fb27745f69f05037d726
Magdalena Noemi Puerto Riegos <103017472+NoemiPuerto@users.noreply.github.com>
2026-04-19 20:17:00 -0500

    cosillas

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -34,9 +34,8 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
     console.log("Registrando comandos...");
 
     await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
+      Routes.applicationCommands(
+        process.env.CLIENT_ID
       ),
       { body: commands }
     );
